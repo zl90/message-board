0ea0c00d1e51087122c82d57e5f1f2f892a1ac15
zl90 <101400977+zl90@users.noreply.github.com>
2022-06-16 21:35:33 +1000

    Initial setup, basic chat functionality

---
+++ routes/index.js
@@ -0,0 +1,48 @@
+var express = require("express");
+var router = express.Router();
+const bodyParser = require("body-parser");
+
+/**
+ * Sample messages.
+ * These are posted at the beginning of a conversation.
+ */
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello, World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+// Allows form values to be parsed by the request object
+router.use(bodyParser.urlencoded({ extended: true }));
+
+/* GET new message */
+router.post("/", (req, res, next) => {
+  const messageText = req.body.messageText;
+  const messageUser = req.body.messageUser;
+
+  messages.push({
+    text: messageText,
+    user: messageUser,
+    added: new Date(),
+  });
+
+  res.render("index", {
+    title: "Z-Messenger",
+    messages: messages,
+    prevUserName: messageUser,
+  });
+});
+
+/* GET home page. */
+router.get("/", (req, res, next) => {
+  res.render("index", { title: "Z-Messenger", messages: messages });
+});
+
+module.exports = router;
